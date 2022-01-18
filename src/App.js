a6b4302dc6eae1c4907c4aebb27c183f47b2b62f
uniqcle <uniqcle@yandex.ru>
2022-01-18 16:18:11 +0700

    class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import Counter from './components/Counter'
+import ClassCounter from './components/ClassCounter';
 
 function App() {
 
   return (
     <div className="App">
-      <Counter />
+      <ClassCounter />
     </div>
   );
 }
