4f935111010df2be8d6f8301b16bae5ec9b5c9fc
uniqcle <uniqcle@yandex.ru>
2022-01-18 15:31:01 +0700

    управляемый input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 function App() {
   const [likes, setLikes] = useState(0)
+  const [value, setValue] = useState('Текст в инпуте')
 
   const increment = () => {
     setLikes(likes + 1)
@@ -16,6 +17,9 @@ function App() {
       <h2>{likes}</h2>
       <button onClick={increment}>+</button>
       <button onClick={decrement}>-</button>
+      <hr />
+      <h2>{value}</h2>
+      <input type="text" value={value} onChange={e => setValue(e.target.value)} />
     </div>
   );
 }
