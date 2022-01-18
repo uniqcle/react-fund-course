dc9ae5fe3266188575c17c1096f5582ca48c3563
uniqcle <uniqcle@yandex.ru>
2022-01-18 17:03:26 +0700

    props

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import React, { useState } from 'react'
 import './styles/App.css'
 import Counter from './components/Counter'
 import ClassCounter from './components/ClassCounter';
+import PostItem from './components/postItem';
 
 function App() {
 
   return (
     <div className="App">
-      <div className="post">
-        <div className="post__content">
-          <strong>1.Javascript</strong>
-          <div>Javascript язык программирования</div>
-        </div>
-        <div className="post__btns">
-          <button>Удалить</button>
-        </div>
-      </div>
+      <PostItem post={{ id: 1, title: 'Js', body: 'description js' }} />
+
     </div>
   );
 }
