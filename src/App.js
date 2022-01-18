6f7f436565f458e4b166d1bd40cf7ea30a7d66d9
uniqcle <uniqcle@yandex.ru>
2022-01-18 15:51:40 +0700

    functional component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React, { useState } from 'react'
+import Counter from './components/Counter'
 
 function App() {
-  const [likes, setLikes] = useState(0)
-  const [value, setValue] = useState('Текст в инпуте')
-
-  const increment = () => {
-    setLikes(likes + 1)
-  }
-
-  const decrement = () => {
-    setLikes(likes - 1)
-  }
 
   return (
     <div className="App">
-      <h2>{likes}</h2>
-      <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
-      <hr />
-      <h2>{value}</h2>
-      <input type="text" value={value} onChange={e => setValue(e.target.value)} />
+      <Counter />
     </div>
   );
 }
