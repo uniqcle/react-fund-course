98f7350de8ccd1b4eed395fc625080ba7ccd2c3a
uniqcle <uniqcle@yandex.ru>
2022-01-18 16:38:27 +0700

    css styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import './styles/App.css'
 import Counter from './components/Counter'
 import ClassCounter from './components/ClassCounter';
 
@@ -6,7 +7,15 @@ function App() {
 
   return (
     <div className="App">
-      <ClassCounter />
+      <div className="post">
+        <div className="post__content">
+          <strong>1.Javascript</strong>
+          <div>Javascript язык программирования</div>
+        </div>
+        <div className="post__btns">
+          <button>Удалить</button>
+        </div>
+      </div>
     </div>
   );
 }
