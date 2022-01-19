8d258d6f93ddd0abb27cd3428036df3c4d9a2a0e
uniqcle <uniqcle@yandex.ru>
2022-01-19 16:36:09 +0700

    refactor postlist

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -3,6 +3,12 @@ import PostItem from './postItem';
 
 function PostList({ posts, title, remove }) {
 
+    if (!posts.length) {
+        return (
+            <h1 style={{ textAlign: 'center' }}>Посты не найдены</h1>
+        )
+    }
+
     return (
         <div>
             <h1 style={{ textAlign: 'center' }}>{title}</h1>
