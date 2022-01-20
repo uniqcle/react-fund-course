ed9fe96f469d47311ea4623c49561e3826bcda58
uniqcle <uniqcle@yandex.ru>
2022-01-20 13:23:33 +0700

    axios

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import PostFilter from './components/PostFilter';
 import MyModal from './components/UI/modal/MyModal';
 import MyButton from './components/UI/button/MyButton';
 import { usePosts } from './hooks/usePosts';
+import axios from 'axios';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -24,12 +25,14 @@ function App() {
 
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
-
-
-
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
     setModal(false)
+  }
+
+  async function fetchPosts() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    setPosts(response.data)
   }
 
   const removePost = (post) => {
@@ -38,6 +41,7 @@ function App() {
 
   return (
     <div className="App">
+      <button onClick={fetchPosts}>Get posts</button>
 
       <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>Создать пользователя</MyButton>
 
