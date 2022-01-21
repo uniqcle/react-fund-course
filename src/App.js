efd38699bc2f69e2ce5ed7678bce3651426e67ec
uniqcle <uniqcle@yandex.ru>
2022-01-21 14:49:42 +0700

    pagination

--- src/App.js
+++ src/App.js
@@ -10,28 +10,34 @@ import { usePosts } from './hooks/usePosts';
 import PostService from './API/PostService'
 import Loader from './components/UI/Loader/Loader'
 import { useFetching } from './hooks/useFetching';
+import { getPageCount, getPagesArray } from './utils/pages';
+import Pagination from './components/UI/pagination/Pagination';
 
 function App() {
   const [posts, setPosts] = useState([])
 
   const [filter, setFilter] = useState({ sort: '', query: '' })
   const [modal, setModal] = useState(false)
-  const [totalCount, setTotalCount] = useState(0)
+  const [totalPages, setTotalPages] = useState(0)
   const [limit, setLimit] = useState(10)
   const [page, setPage] = useState(1)
 
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
+
+
   const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
     const response = await PostService.getAll(limit, page);
     setPosts(response.data)
-    console.log(response.headers['x-total-count'])
-    setTotalCount(response.headers['x-total-count'])
+    const totalCount = response.headers['x-total-count']
+    setTotalPages(getPageCount(totalCount, limit))
   })
+
+  console.log(totalPages)
 
   useEffect(() => {
     fetchPosts();
-  }, []);
+  }, [page]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -40,6 +46,11 @@ function App() {
 
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
+  }
+
+  const changePost = page => {
+    setPage(page)
+
   }
 
   return (
@@ -64,6 +75,8 @@ function App() {
         : <PostList posts={sortedAndSearchedPosts} remove={removePost} title="Список постов" />
       }
 
+
+      <Pagination page={page} changePost={changePost} totalPages={totalPages} />
 
     </div>
   );
